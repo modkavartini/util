4dc44f3feae32987329833993a7643ccfc9965f6
abhiram <81793953+modkavartini@users.noreply.github.com>
2024-02-28 03:12:19 +0530

    added p and attemptIn

--- make.ps1
+++ make.ps1
@@ -1,3 +1,5 @@
+$path = "$env:userProfile\Documents\psd"
+
 function make {
     param (
         [string]
@@ -9,9 +11,9 @@ function make {
         [string]
         $title = "template.psd",
         [switch]
-        $clear,
+        $continue,
         [string]
-        $path = "$env:userProfile\Documents\psd"
+        $path = $path
     )
 
     if (($text -eq "null") -or (!(get-Process "Photoshop" -eA 0))) {
@@ -24,14 +26,10 @@ function make {
     write-Host "$preview" -foregroundColor yellow
     "`n"
 
-    if ($clear) {
+    if (!($continue)) {
         if (test-Path $path) {
-            write-Host "clearing output folder..." -foregroundColor red
+            write-Host "clearing output folder...`n" -foregroundColor red
             get-ChildItem $path -filter c*.png | forEach-Object { $_.delete() }
-        }
-        else {
-            write-Error "defined output path was not found!"
-            break
         }
     }
 
@@ -39,8 +37,8 @@ function make {
         get-ChildItem $path -filter c*.png | forEach-Object { $start++ }
     }
 
-    write-Host "`npress any key to start...`n" -foregroundColor red
-    $null = $host.UI.rawUI.readKey('noEcho,includeKeyDown')
+    #write-Host "`npress any key to start...`n" -foregroundColor red
+    #$null = $host.UI.rawUI.readKey('noEcho,includeKeyDown')
 
     for ($t = 5; $t -gt 1; $t--) {
         write-Progress -activity " starting" -status "in:" -secondsRemaining $t
@@ -109,9 +107,9 @@ function make {
     waitFor 1
     nircmd.exe win settopmost ititle "$title" 0
     nircmd.exe win min ititle "$title"
-    nircmd.exe infobox "tasks completed!" "done!"
+    #nircmd.exe infobox "tasks completed!" "done!"
     waitFor 1
-    nircmd.exe win settopmost title "done!" 1
+    #nircmd.exe win settopmost title "done!" 1
     write-Host "completed all $($i-1) tasks!" -foregroundColor red
 }
 
@@ -127,19 +125,22 @@ function waitFor($t) {
 function grab {
     param (
         [int32]
-        $c = 0
+        $c = 0,
+        [switch]
+        $alive
     )
     $result = ""
 
-    #nircmd.exe win activate ititle "Profile"
     nircmd.exe win max ititle "Profile"
     waitFor 1
     sendKey 0x24
-    waitFor 2
+    sendKey 0x11+0x74
+    waitFor 12
+    if (!($alive)) { attemptIn }
     nircmd.exe setcursor 320 958
-    #320 945
 
     for ($j = 0; $j -lt $c; $j++) {
+        waitFor 1
         nircmd.exe sendmouse left dblclick
         nircmd.exe sendmouse left dblclick
         #sendKey shift+0x23
@@ -163,4 +164,82 @@ function nextGrab {
         sendKey 0x28
         waitFor 0.1
     }
+}
+
+function attemptIn {
+    goAndClick -x 960 -y 295
+    waitFor 1
+    sendKey 0x43
+    sendKey 0x4F
+    sendKey 0x4E
+    sendKey 0x46
+    sendKey 0x45
+    sendKey 0x53
+    sendKey 0x53
+    sendKey 0x49
+    sendKey 0x4F
+    sendKey 0x4E
+    sendKey 0x53
+    sendKey 0x4F
+    sendKey 0x46
+    sendKey 0x43
+    sendKey 0x45
+    sendKey 0x4B
+    waitFor 1
+    sendKey enter
+    waitFor 1
+    sendKey 0x41
+    sendKey 0x41
+    sendKey 0x52
+    sendKey 0x59
+    sendKey 0x41
+    sendKey 0x4E
+    sendKey shift+0x33
+    sendKey 0x41
+    sendKey 0x42
+    sendKey 0x48
+    sendKey 0x49
+    sendKey 0x52
+    sendKey 0x41
+    sendKey 0x4D
+    sendKey enter
+}
+
+function goAndClick {
+    param (
+        [int32]
+        $x,
+        [int32]
+        $y
+    )
+    nircmd.exe setcursor $x $y
+    nircmd.exe sendmouse left click
+    waitFor 2
+}
+
+function p {
+    param (
+        [int32]
+        $c = 1
+    )
+    waitFor 2
+    nircmd.exe win max ititle "Instagram"
+    waitFor 2
+    for ($l = 1; $l -le $c; $l++) {
+        goAndClick -x 170 -y 690
+        goAndClick -x 960 -y 700
+        waitFor 1
+        set-Clipboard "$path\c$l.png"
+        sendKey ctrl+v
+        sendKey enter
+        waitFor 2
+        goAndClick -x 1260 -y 245
+        goAndClick -x 1470 -y 245
+        goAndClick -x 1470 -y 245
+        waitFor 5
+        goAndClick -x 1865 -y 145
+        write-Host "p'd c$($l)!`n" -foregroundColor green
+        waitFor 3
+    }
+    nircmd.exe win min ititle "Instagram"
 }
